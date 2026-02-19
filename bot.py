9bd53a7061bae22bdef9d799498db8ccbc949f9e
y617101 <48057966+y617101@users.noreply.github.com>
2026-02-20 02:09:47 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
 import os
 import json
+import requests
 import gspread
 from google.oauth2.service_account import Credentials
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
+
 
 
 
